2eefc3548aefb0b7facfd2a6c0147a33a1e2e5d4
gurushant <mattigurushant@gmail.com>
2025-07-19 21:54:35 +0530

    ui done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,27 @@ import MoodSongs from './components/MoodSongs'
 function App() {
 
 
+    const [ Songs, setSongs ] = useState([
+        // {
+        //     title: "test_title",
+        //     artist: "test_artist",
+        //     url: "test_url",
+        // },
+        // {
+        //     title: "test_title",
+        //     artist: "test_artist",
+        //     url: "test_url",
+        // },
+        // {
+        //     title: "test_title",
+        //     artist: "test_artist",
+        //     url: "test_url",
+        // },
+    ])
   return (
     <>
-      <FacialExpression />
-      <MoodSongs />
+      <FacialExpression setSongs={setSongs} />
+      <MoodSongs Songs={Songs} />
     </>
   )
 }
